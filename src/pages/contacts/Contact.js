19b5f42e8cb426b1972025bfb4f099d86f262fec
René Alexander Rivera Hernández <alexxanderrivas@gmail.com>
2022-10-04 20:28:11 -0600

    v1

---
+++ src/pages/contacts/Contact.js
@@ -0,0 +1,11 @@
+export default function Contact(){
+    return(
+        <header className="container">
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <h2>Hola mundo esto es el contacto</h2>
+        </header>
+    )
+}
